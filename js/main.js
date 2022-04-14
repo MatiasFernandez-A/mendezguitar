5412b1cfae95570ca98299a039410f9c95b88ad8
Matias Fernandez <fernandezalmedamatias@gmail.com>
2022-04-14 20:42:15 -0300

    Agregando metodo length en array

--- js/main.js
+++ js/main.js
@@ -12,25 +12,39 @@ class Productos {
         this.vendido = true;
     }
 }
-let aniadirCarrito = " ";
+
+let aniadirCarrito = [] ;
 
 /* funcio inventario: Pide valores para varibles guitarra y precio. Las guarda por parametro en la clase Productos creando un nuevo producto */
+
 function inventario() {
     let guitarra = prompt("Que guitarra queres?");   
     let precio = parseInt(prompt("Ingrese precio de la guitarra. El precio se muestra en la tabla de precios"));   
-    const producto1 = new Productos(guitarra,precio);
-    producto1.sumarIva();
-    producto1.vender();
-    console.log(producto1)
+    let nuevoProd = new Productos(guitarra,precio); 
+    nuevoProd.sumarIva();
+    nuevoProd.vender();
+    let carrito = confirm("Queres añadirlo al carrito ?");
+    if (carrito == true){
+        aniadirCarrito.push(nuevoProd);
+        console.log(aniadirCarrito);
+    }
+    
+    console.log(nuevoProd);
 }
+
 
 inventario();
 
-aniadirCarrito = confirm("Queres añadirlo al carrito?");
+/* Queres saber si la guitarra que elegiste esta en el carrito? */
 
-const carrito = [];
+let cantidadCarrito = confirm("Queres saber cuantos productos tenes en el carrito?")
 
-if (aniadirCarrito == true){
-carrito.push(new producto1);
+let cantidad = () => {
+    if (cantidadCarrito == true) {
+        console.log( aniadirCarrito.length );
+    } else {
+        console.log ("No se muestra")
+    }
 }
-console.log(carrito);
+
+cantidad();
