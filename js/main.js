96e0b47edd9406eb39a4223a41f3907fe66d2f84
Matias Fernandez <fernandezalmedamatias@gmail.com>
2022-04-25 12:49:53 -0300

    Cabio total

--- js/main.js
+++ js/main.js
@@ -13,9 +13,12 @@ class Productos {
     }
 }
 
+
 let aniadirCarrito = [] ;
 let nuevoProd = 0; 
+
 /* funcio inventario: Pide valores para varibles guitarra y precio. Las guarda por parametro en la clase Productos creando un nuevo producto */
+
 function inventario(){
     let guitarra = document.getElementById("guitarraModelo").value;
     let precio = document.getElementById("precioGuitarra").value;
@@ -23,7 +26,6 @@ function inventario(){
     nuevoProd.sumarIva();
     nuevoProd.vender()
 }
-
 inventario();
 
 const boton = document.querySelector("#btn");
@@ -37,11 +39,19 @@ boton.addEventListener("click", (e) => {
 
 
 
+/* En clase 9 esta la funcion para hacer aparecer carrito cuando le añado un producto  */
+
+
+// Funcionalidades para agregar al e-comerce
+
+/* funcion para filtrar por el buscador el producto que busquemos  */
+/* funcion para mostrar un historial de los productos que buscamos */
+/* funcio para agregar productos favoritos  */
+/* hacer que aparezca el stock disponible del producto  */
+/* funcion para agregar cantidad de productos al carrito y a medida que aumento la cantidad baja la cantidad de stock */
 
 
 
 
-
-
-
-
+/* Una api es una respuesta del servidor que nos va a estar entregando datos que nosotros podemos usar en nuestra app web*/
+/* se usa fetch para leer apis */
