f1573c05d2780abcc616ac6eb90c244ad70d275c
Matias Fernandez <fernandezalmedamatias@gmail.com>
2022-05-17 22:54:36 -0300

    Modal

--- js/maintres.js
+++ js/maintres.js
@@ -1,13 +1,13 @@
 const contenedorProductos = document.getElementById("contenedor-productos");
 
 const contenedorCarrito = document.getElementById("carrito-contenedor");
-
+// no eliminar
 const botonVaciar = document.getElementById("vaciar-carrito");
-
+// no eliminar
 const contadorCarrito = document.getElementById("contadorCarrito");
 
 const precioTotal = document.getElementById("precioTotal");
-
+// no eliminar
 
 
 let carrito = [];
